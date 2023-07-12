aeb0310fd0f19311ba5cd52918df5d0dadb14010
Manuel Tarby <manuel.tarby@gmail.com>
2023-07-12 18:14:10 +0200

    Colorisation des boutons "Supprimer".

--- GestionLocation/Locataires.Designer.cs
+++ GestionLocation/Locataires.Designer.cs
@@ -43,12 +43,15 @@ namespace GestionLocation
             // 
             // btnSupprimer
             // 
+            this.btnSupprimer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.btnSupprimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSupprimer.ForeColor = System.Drawing.SystemColors.Control;
             this.btnSupprimer.Location = new System.Drawing.Point(306, 18);
             this.btnSupprimer.Name = "btnSupprimer";
             this.btnSupprimer.Size = new System.Drawing.Size(132, 48);
             this.btnSupprimer.TabIndex = 13;
             this.btnSupprimer.Text = "Supprimer";
-            this.btnSupprimer.UseVisualStyleBackColor = true;
+            this.btnSupprimer.UseVisualStyleBackColor = false;
             this.btnSupprimer.Click += new System.EventHandler(this.BtnSupprimer_Click);
             // 
             // btnModifier
