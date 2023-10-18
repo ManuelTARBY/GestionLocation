37a8e5347e99e1646377e67b60868043301adacd
Manuel Tarby <manuel.tarby@gmail.com>
2023-10-18 16:26:13 +0200

    Passage de la liste des locations en cours à non cliquable (fenêtre Accueil).

--- GestionLocation/Accueil.Designer.cs
+++ GestionLocation/Accueil.Designer.cs
@@ -55,6 +55,7 @@ namespace GestionLocation
             this.lstLocations.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstLocations.Enabled = false;
             this.lstLocations.FormattingEnabled = true;
             this.lstLocations.ItemHeight = 20;
             this.lstLocations.Location = new System.Drawing.Point(3, 215);
